8e297acfa229b866bc940d788d89162f66294b05
mohammed fariz <mohmd.fariz@gmail.com>
2022-03-25 07:34:12 +0530

    Updates for v0.10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## 0.1.0
+
+- Update config file
+- Update index file
+- Update script file
+- Update package json file
+
 ## 0.0.1
 
 - Initial Commit
--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const packageJson = require('./../package.json');
-const { getStatus, enable, disable } = require('../lib/script');
 
 const commands = ['add', 'remove', 'status'];
 const optionsLength = {
@@ -19,19 +18,19 @@ const isValidCommand = () => {
   return commands.indexOf(command) > -1 && process.argv.length <= optionsLength[command];
 };
 
-console.log('                          ---------------------------------- ');
-console.log(`                         |       Start-On-Boot v${packageJson.version}       |`);
-console.log('                          ---------------------------------- ');
+console.log('                               ---------------------------------- ');
+console.log(`                              |        n-bootstart v${packageJson.version}        |`);
+console.log('                               ---------------------------------- ');
 if (isValidCommand()) {
   require('../lib/process-command');
 } else {
-  console.log('Usage: start-on-boot <command> [arg1] [arg2] \n');
-  console.log("start-on-boot add [project-name] [path]                        enables the start-on-boot for the provided project, so that the project will be started when the system starts up\n\n");
+  console.log('Usage: n-bootstart <command> [arg1] [arg2] \n');
+  console.log("n-bootstart add [project-name] [path]                       starts the provided project when the system starts up\n");
+  console.log("n-bootstart remove [project-name] [path]                    disables the n-bootstart for the provided project\n\n");
   console.log('Examples: \n');
-  console.log("start-on-boot add gateway-app D:\/projects\/gateway-backend      enables the start-on-boot for the 'gateway app' project\n");
+  console.log("n-bootstart add gateway-app D:\/gateway-backend              starts the 'gateway-app' project when the system starts up\n");
+  console.log("n-bootstart remove gateway-app D:\/gateway-backend           disables the n-bootstart for the 'gateway-app' project\n");
 }
 
-module.exports = {
-  getStatus, enable, disable
-};
+require('../lib/script');
 
